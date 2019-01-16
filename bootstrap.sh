c9f128573afde5d0af52ffce9351325145b85cd3
Jan Schnasse <schnasse@gmx.de>
2019-01-16 09:29:47 +0100

    remove regal-start script

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,12 @@ sudo bin/plugin -install mobz/elasticsearch-head
 sudo bin/plugin install elasticsearch/elasticsearch-analysis-icu/2.1.0
 sudo bin/plugin -install com.yakaz.elasticsearch.plugins/elasticsearch-analysis-combo/1.5.1
 
-cd /opt/regal/
+sudo mkdir /opt/regal
+sudo chown -R vagrant /opt/regal/
+mkdir /opt/regal/src
+mkdir /opt/regal/apps
+
+cd /opt/regal/src
 git clone https://github.com/edoweb/regal-api
 git clone https://github.com/edoweb/regal-install
 git clone https://github.com/hbz/thumby
@@ -39,8 +44,28 @@ cp /vagrant/typesafe-activator-1.3.5.zip .
 
 ln -s /opt/regal/activator-dist-1.3.5 /opt/regal/activator
 
-sudo adduser --home /home/hbz hbz
-sudo mkdir /opt/regal
-sudo chown -R hbz /opt/regal/
-sudo su -l hbz
+
+function install(){
+VERSION=$1
+APPNAME=$2
+/opt/activator/bin/activator clean
+/opt/activator/bin/activator dist
+cp target/universal/$VERSION.zip  /tmp
+cd /tmp
+unzip $VERSION.zip
+mv $VERSION /opt/regal/apps/$APPNAME
+}
+
+cd /opt/regal/src/thumby;
+install thumby-0.1.0-SNAPSHOT thumby
+
+cd /opt/regal/src/etikett
+install etikett-0.1.0-SNAPSHOT etikett
+
+cd /opt/regal/src/zettel
+install zettel-0.1.0-SNAPSHOT zettel
+
+cd /opt/regal/src/regal-api
+install regal-api-0.1.0-SNAPSHOT regal-api
+
 
